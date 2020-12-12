6555ca0c4b31c55220ec6778142908acc776a9a2
loser968 <mproshan968@gmail.com>
2020-12-12 21:25:38 +0530

    added spring-security for rest-based-spring-boot-app project

--- Roshan_856667/MorningSession/Spring-Boot-Exercise/rest-based-spring-boot-app.zip_expanded/rest-based-spring-boot-app/src/main/java/com/org/controller/CustomerController.java
+++ Roshan_856667/MorningSession/Spring-Boot-Exercise/rest-based-spring-boot-app.zip_expanded/rest-based-spring-boot-app/src/main/java/com/org/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.org.controller;
 
 
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.org.Customer;
@@ -70,5 +72,10 @@ public class CustomerController {
 		
 	}
 	
+	@RequestMapping("user")
+	@ResponseBody
+	public Principal user(Principal principal) {
+		return principal;
+	}
 	
 }
